9637f37eb7340bc24cbaacc88299055e418789d9
SouravKKR <souravprogramming@gmail.com>
2024-11-09 19:35:53 +0530

    Some commit

--- Components/HeaderComponent.js
+++ Components/HeaderComponent.js
@@ -1,3 +1,5 @@
+import { theme } from '../Common/Constants/Theme.js';
+
 const express = require('express');
 
 class HeaderComponent extends HTMLElement
@@ -19,6 +21,20 @@ class HeaderComponent extends HTMLElement
             <img style="margin:5px;" height="50%" width="auto" src="https://img.freepik.com/premium-vector/decorative-square-frame-border-with-geometric-lines-symmetrical-pattern-outlined-shape_1226483-15150.jpg">
             <h1 style="margin:5px;">Cipher Box</h1>
         `;
+        
+        const backButton = document.createElement("button");
+        backButton.innerText = "Back";
+        backButton.style.color = "white";
+        backButton.style.border = "solid white 2px";
+        backButton.style.backgroundColor = theme.secondaryBackgroundColor;
+        backButton.style.display = "inline-block";
+        backButton.style.left = backButton.style.top = "5%";
+        backButton.style.width = "100px"
+        backButton.style.height = "30px"
+        backButton.style.position = "fixed";
+
+        backButton.onclick = ()=>{ history.back(); };
+        document.body.appendChild(backButton);
     }
 }
 
--- Pages/HostServer.js
+++ Pages/HostServer.js
@@ -3,6 +3,5 @@ import { selectionModes } from "../Common/Constants/SelectionModes.js";
 const hostServerPopup = document.querySelector(".host-server-popup");
 
 const serverPathFileSelector = document.createElement("file-selector");
-
 serverPathFileSelector.initialize(selectionModes.FOLDER, "");
 hostServerPopup.appendChild(serverPathFileSelector);
