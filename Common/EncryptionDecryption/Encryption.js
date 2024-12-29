4429262c0f711fd661364871d52964c4d3dd85c9
Sooraj Sajjan <soorajsajjan22@gmail.com>
2024-12-29 19:38:51 +0530

    Some commit

--- Common/EncryptionDecryption/Encryption.js
+++ Common/EncryptionDecryption/Encryption.js
@@ -1,6 +1,52 @@
-import {EncryptionDecryption} from '../EncryptionDecryption/EncryptionDecryption';
-class Encryption extends EncryptionDecryption
-{
+// import {EncryptionDecryption} from '../EncryptionDecryption/EncryptionDecryption';
+// import crypto from 'crypto';
+const crypto =require('crypto');
+// class Encryption //extends EncryptionDecryption
+// {
+//     // constructor()
+//     // {
+//     //     super();
+//     //     this.levels=[]
+//     // }
+    
+  
+// }
+// export default Encryption;
 
-}
-export default Encryption;
+
+const key = crypto.randomBytes(32);  
+const iv = crypto.randomBytes(16); 
+
+// AES Encryption function
+const AES_Encryption = (data) => {
+    if (!(data instanceof Uint8Array)) {
+        throw new Error('Input must be a Uint8Array');
+    }
+
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);  
+    const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);  
+    return encrypted; 
+};
+
+let pt = "Hello world";
+// const originalData = Buffer.from(pt, 'utf-8'); 
+const originalData = new TextEncoder().encode(pt);
+// Encrypt the data
+const encryptedData = AES_Encryption(originalData);
+
+console.log(encryptedData.toString('utf-8'));
+console.log('Encrypted data successfully processed.');
+
+// AES Decryption function
+const AES_Decryption = (data) => {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    const decrypted = Buffer.concat([decipher.update(data), decipher.final()]);
+    return decrypted;
+};
+
+const decryptedData = AES_Decryption(encryptedData);
+
+const finalText = decryptedData.toString('utf-8');  
+console.log(finalText); 
+console.log("Successfully Decrypted");
+
---
+++ Common/EncryptionDecryption/temp_rsa.js
@@ -0,0 +1,19 @@
+const crypto=require('crypto');
+const{ publicKey,privateKey}=crypto.generateKeyPairSync('rsa',{modulusLength:1024});
+const RSA_Encryption=(data)=>
+{
+    return crypto.publicEncrypt(publicKey,data);
+}
+const RSA_Decryption=(data)=>
+{
+    return crypto.privateDecrypt(privateKey,data);
+}
+let pt="Hello world";
+const originalData=new TextEncoder().encode(pt);
+const encryptedData=RSA_Encryption(originalData);
+console.log(encryptedData);
+console.log(encryptedData.toString('utf-8'));
+console.log("\n");
+const decryptedData=RSA_Decryption(encryptedData);
+console.log(decryptedData);
+console.log(decryptedData.toString('utf-8'));
